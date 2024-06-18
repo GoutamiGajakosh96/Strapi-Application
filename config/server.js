9254e4ca8fe58ab09443e7594129bde260c8828f
GoutamiGG <goutamigajakosh100@gmail.com>
2024-06-18 22:14:45 +0530

    changes

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: 'yDrULkmPayl4Qgg5r6YSoQ==',
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
